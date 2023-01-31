e4ac0077b644d0bde18f524424e2d1135c2bb228
Maxbwee <maxbowen1234@gmail.com>
2023-01-31 10:05:42 +0200

    Katas

---
+++ noStrangers.js
@@ -0,0 +1,19 @@
+let str = ""
+
+const noStrangers = (str) => {
+
+    
+    
+    const acquaintance = [];
+    const friend = [];
+
+    let sentence = str.split(' ')
+    
+    
+    
+
+    console.log(sentence)
+
+}
+
+console.log(noStrangers("Hello, my name is Max"))
